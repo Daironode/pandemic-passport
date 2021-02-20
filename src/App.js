1bffdb6f7ccaace6dafa6280700afcc289779c68
Hugo Guerrero <hugo.guerrero@ariwonto.com>
2021-02-19 23:12:31 -0500

    Space

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './components/home/HomeStyles.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../src/components/home/home';
-import MapView from './components/map/MapView';
+import MapView from '../src/components/map/mapView';
 
 function App() {
   return (
--- src/components/space/space.js
+++ src/components/space/space.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './spaceStyles.css';
 
 export default class Spaces extends Component {
     constructor(props) {
@@ -10,11 +11,86 @@ export default class Spaces extends Component {
 
     render = () => {
         return (
-            <div>
-                <div className="title">
+            <div className="">
+                <div className="title" >
                     Space
                 </div>
-                <div className="body">
+                <div className="content">
+                <div className="title" >
+                    
+                    Identification Your Space
+                </div>
+                <div className="contentRow">
+
+                    <div className="group">
+                        <label className="labelInput">Document Identification</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                    <div className="group">
+                        <label className="labelInput">Name Space</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                </div>
+
+                <div className="contentRow">
+
+                    <div className="group">
+                        <label className="labelInput">Document Identification</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                    <div className="group">
+                        <label className="labelInput">Name Space</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                </div>
+
+                <div className="contentRow">
+
+                    <div className="group">
+                        <label className="labelInput">Document Identification</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                    <div className="group">
+                        <label className="labelInput">Name Space</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                </div>
+
+                <div className="contentRow">
+
+                    <div className="group">
+                        <label className="labelInput">Document Identification</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                    <div className="group">
+                        <label className="labelInput">Name Space</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                </div>
+
+                <div className="contentRow">
+
+                    <div className="group">
+                        <label className="labelInput">Document Identification</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                    <div className="group">
+                        <label className="labelInput">Name Space</label>
+                        <input type="text" name=" " id="space" className="input"/>
+                    </div>
+
+                </div>
+
+                    
 
                 </div>
             </div>
